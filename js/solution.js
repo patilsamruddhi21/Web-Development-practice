63e5d58122fdeac202022ecc600b994753a586dd
Samruddhi <patilsamruddhi4747@gmail.com>
2022-12-10 17:21:48 +0530

    Initial commit

---
+++ js/solution.js
@@ -0,0 +1,45 @@
+
+// Task 1
+
+function logDairy() {
+    const dairy = [
+      "cheese",
+      "sour cream",
+      "milk",
+      "yogurt",
+      "ice cream",
+      "milkshake",
+    ];
+  
+    for (var item of dairy) {
+      console.log(item);
+    }
+  }
+  logDairy();
+  
+  // Task 2
+  const animal = {
+    canJump: true,
+  };
+  
+  const bird = Object.create(animal);
+  bird.canFly = true;
+  bird.hasFeathers = true;
+  
+  function birdCan() {
+    for (key of Object.keys(bird)) {
+      console.log(key + ": " + bird[key]);
+    }
+  }
+  birdCan();
+  
+  // Task 3
+  
+  function animalCan() {
+    for (prop in bird) {
+      console.log(prop + ": " + bird[prop]);
+    }
+  }
+  
+  animalCan();
+  
